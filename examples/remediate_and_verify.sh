54b536c4897d1d49e6838399d0a9ab7dc4d2463a
Him Kalyan Bordoloi <bordoloih@vmware.com>
2020-04-24 21:48:34 +0000

    Add remaining CIS 1 scored scripts

--- examples/remediate_and_verify.sh
+++ examples/remediate_and_verify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-mount --rbind /dev /root/testroot/dev
+mount -t proc proc /root/testroot/proc/
+mount --rbind /sys /root/testroot/sys/
+mount --rbind /dev /root/testroot/dev/
 pushd /src
 ansible-playbook  -i examples/chroots cis.playbook.yml --tags "cis" \
 && \
